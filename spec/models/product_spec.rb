d6d3ceb103c48b16221cdc8bb0144b47d1cdb385
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-04-21 14:44:57 -0400

    add image of product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -11,5 +11,7 @@ RSpec.describe Product, type: :model do
   it { is_expected.to belong_to :productable }
   it { is_expected.to have_many(:product_categories).dependent(:destroy) }
   it { is_expected.to have_many(:categories).through(:product_categories) }
+
+  it { is_expected.to validate_presence_of(:image) }
 end
 
