f3b75a1a83e43d1b00626946198973b3a54963fa
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-04-23 11:24:05 -0400

    test  post category

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -17,4 +17,48 @@ RSpec.describe "Admin::V1::Categories", type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  context "POST /categories" do
+    let(:url) { "/admin/v1/categories" }
+
+    context "with valid params " do
+      let(:category_params) { { attributes_for(:category)}.to_json }
+
+      it "adds a new Category" do
+        expect do 
+          post url, headers: auth_header(user), params: category_params
+          end.to change(Category, :count).by(1)
+        end
+      end
+
+      it 'returns last added Category' do
+        post url, headers: auth_header(:user), params: category_params
+        expect_category = Category.last.as_json(only: i%(id name))
+        expect(body_json['category']).to eq expect_category
+      end
+
+      it "returns success status" do
+        post url, headers: auth_header(user), params: category_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+    
+    context "with invalid params " do
+      let(:category_invalid_params) do
+        { category: attributes_for(:category, name: nil) }.to_json 
+      end
+
+      it 'does not add a new category'
+        expect do
+          post url, headers: auth_header(user), params: category_invalid_params
+        end.to_not change(Category, :count)
+      end
+
+      it 'returns error messages' do
+        post url, headers: auth_header(user), params: category_invalid_params
+        expect(body_json['errors']['fileds']).to have_key('name')
+      end
+    end
+  end
+
 end
