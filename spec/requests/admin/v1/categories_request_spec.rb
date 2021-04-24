28c622a1f260ff5c3671c2a529def85865f75146
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-04-24 09:21:01 -0400

    message error

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -24,43 +24,43 @@ RSpec.describe "Admin::V1::Categories", type: :request do
     context "with valid params " do
       let(:category_params) { { category: attributes_for(:category) }.to_json }
 
-      it "adds a new Category" do
-        expect do 
+      it 'adds a new Category' do
+        expect do
           post url, headers: auth_header(user), params: category_params
         end.to change(Category, :count).by(1)
       end
 
       it 'returns last added Category' do
-        post url, headers: auth_header(:user), params: category_params
-        expect_category = Category.last.as_json(only: i%(id name))
-        expect(body_json['category']).to eq expect_category
+        post url, headers: auth_header(user), params: category_params
+        expected_category = Category.last.as_json(only: %i(id name))
+        expect(body_json['category']).to eq expected_category
       end
 
-      it "returns success status" do
+      it 'returns success status' do
         post url, headers: auth_header(user), params: category_params
         expect(response).to have_http_status(:ok)
       end
     end
     
     context "with invalid params " do
-      let(:category_invalid_params) do
-        { category: attributes_for(:category, name: nil) }.to_json 
+      let(:category_invalid_params) do 
+        { category: attributes_for(:category, name: nil) }.to_json
       end
 
-      it 'does not add a new category'
+      it 'does not add a new Category' do
         expect do
           post url, headers: auth_header(user), params: category_invalid_params
         end.to_not change(Category, :count)
       end
 
-      it 'returns error messages' do
+      it 'returns error message' do
         post url, headers: auth_header(user), params: category_invalid_params
-        expect(body_json['errors']['fileds']).to have_key('name')
+        expect(body_json['errors']['fields']).to have_key('name')
       end
 
       it 'returns unprocessable_entity status' do
         post url, headers: auth_header(user), params: category_invalid_params
-        expect(reponse).to have_http_status(:unprocessable_entity)
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
