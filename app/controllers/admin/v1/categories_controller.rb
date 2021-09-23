ed36cca7782243cbbadbc705a0598db6d77e3ceb
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-09-23 09:01:33 -0400

    Feat Product Saving

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,6 +1,6 @@
 module Admin::V1
   class CategoriesController < ApiController
-    before_action :load_category, only: [:update, :destroy]
+    before_action :load_category, only: [:show, :update, :destroy]
 
     def index
       @categories = load_categories
